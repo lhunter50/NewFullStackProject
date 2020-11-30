d788b81b7c287600bab96981f53b21386a156ba0
lhunter50 <lhunter50@academic.rrc.ca>
2020-11-30 10:58:37 -0800

    adding add to cart and remove to cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,2 +1,15 @@
 class CartController < ApplicationController
+  def create
+    id = params[:id].to_i
+    session[:shopping_cart] << id unless session[:shopping_cart].include?(id)
+    # flash[:notice] = "item added to cart!" unless session[:shopping_cart].include?(id)
+    redirect_to root_path
+  end
+
+  def destroy
+    id = params[:id].to_i
+    session[:shopping_cart].delete(id)
+
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   end
   resources :contact, only: [:index, :show]
   resources :about, only: [:index, :show]
+  resources :cart, only: [:create, :destroy]
   root to: 'products#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
