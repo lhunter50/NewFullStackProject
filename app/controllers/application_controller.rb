0a76fb534e8fdd43e00c410b7cc8bdf933a1b74f
lhunter50 <lhunter50@academic.rrc.ca>
2020-11-30 10:16:05 -0800

    initialize session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :initialize_session
+
+  private
+
+  def initialize_session
+    session[:visit_count] ||= 0
+  end
+
+  def increase_visits
+
+  end
+
+  def visit_count
+
+  end
+
 end
