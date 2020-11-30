d3e1208df56139fbca39ade2ef1d20fcb3a39c80
lhunter50 <lhunter50@academic.rrc.ca>
2020-11-30 12:23:02 -0800

    fixed stupid active admin

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,7 +2,7 @@ class CartController < ApplicationController
   def create
     id = params[:id].to_i
     session[:shopping_cart] << id unless session[:shopping_cart].include?(id)
-    # flash[:notice] = "item added to cart!" unless session[:shopping_cart].include?(id)
+    flash[:notice] = "Item added to cart!"
     redirect_to root_path
   end
 
