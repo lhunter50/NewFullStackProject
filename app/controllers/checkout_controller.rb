8433b47f1066f766ee66eaa849f74cfb9a8bdc06
lhunter50 <lhunter50@academic.rrc.ca>
2020-12-01 15:20:50 -0800

    added stripe checkout prototype

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,5 +1,33 @@
 class CheckoutController < ApplicationController
   def create
+    product = Product.find(params[:id])
+
+    if product.nil?
+      redirect_to root_path
+      return
+    end
+
+    #connection
+
+    @session = Stripe::Checkout::Session.create(
+      payment_method_types: ["card"],
+      success_url: checkout_success_url,
+      cancel_url: checkout_cancel_url,
+      line_items: [
+        {
+          name: product.name,
+          description: product.description,
+          amount: (product.price * 100).to_i,
+          currency: "cad",
+          quantity: 1
+        }
+      ]
+    )
+
+    #redirect
+    respond_to do |format|
+      format.js
+    end
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,9 @@ Rails.application.routes.draw do
   resources :cart, only: [:create, :destroy]
 
   scope "/checkout" do
-    post "create", to: "checkout#create", as "checkout_create"
-    get "success" to: "checkout#success", as "checkout_success"
-    get "cancel" to: "checkout#cancel" as "checkout_cancel"
+    post "create", to: "checkout#create", as: "checkout_create"
+    get "success", to: "checkout#success", as: "checkout_success"
+    get "cancel", to: "checkout#cancel", as: "checkout_cancel"
   end
 
   root to: 'products#index'
