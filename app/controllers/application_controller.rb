7518a3c7c3046df67e9396fee1f1c572ab7cee13
lhunter50 <lhunter50@academic.rrc.ca>
2020-12-01 18:31:42 -0800

    you can change province via admin now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :initialize_session
-  helper_method :cart
+  helper_method :cart, :province_selection
+  protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+       def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:address, :email, :password)}
+
+            devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:address, :email, :password, :current_password, :province_id)}
+       end
   private
 
   def initialize_session
@@ -11,4 +21,5 @@ class ApplicationController < ActionController::Base
   def cart
     Product.find(session[:shopping_cart])
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_001025) do
+ActiveRecord::Schema.define(version: 2020_12_02_005104) do
 
   create_table "abouts", force: :cascade do |t|
     t.string "title"
@@ -143,6 +143,7 @@ ActiveRecord::Schema.define(version: 2020_12_02_001025) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.text "address"
     t.index ["email"], name: "index_users_on_email"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
